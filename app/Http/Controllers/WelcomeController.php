2bacc1fecc62c8b9e776d1c15c3fa6439ba48260
David <david@docampo.ch>
2020-11-18 18:43:53 +0100

    passing User to view and added some content to the Welcome page

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class WelcomeController extends Controller
 {
     public function view()
     {
-        return Inertia::render('Welcome');
+        return Inertia::render('Welcome', [
+            'User' => Auth::user(),
+        ]);
     }
 }
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,14 +2,25 @@
   <app-layout>
     <template #header>
       <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        Portfolio David Docampo
+        Welcome
       </h2>
     </template>
 
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          Test
+          <div class="p-4">
+            <div class="py-2 border-b border-gray-200">
+              <h2 class="text-2xl font-semibold" v-text="'Hello ' + User.name"></h2>
+            </div>
+            <div class="py-4">
+              <p class="text-xl mb-2">Hey! You made it to my portfolio!</p>
+              <p>I welcome you to my little website. Feel free to explore everything you can get to, maybe you will even find some secrets 😉</p>
+            </div>
+            <div class="py-4">
+              <p class="text-xl mb-2">What you can look at</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -23,6 +34,14 @@ export default {
   name: "Welcome",
     components: {
       AppLayout,
+    },
+    props: {
+        User: {
+            type: Object,
+            default: {
+                name: 'Guest'
+            }
+        }
     }
 }
 </script>
