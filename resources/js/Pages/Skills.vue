79905c6d647e0d991aa0cd4698e0aa2687eefd42
David <david@docampo.ch>
2020-11-18 20:49:19 +0100

    added basic skills layout

--- resources/js/Pages/Skills.vue
+++ resources/js/Pages/Skills.vue
@@ -1,10 +1,29 @@
 <template>
+  <app-layout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Skills
+      </h2>
+    </template>
 
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+        </div>
+      </div>
+    </div>
+  </app-layout>
 </template>
 
 <script>
+import AppLayout from "@/Layouts/AppLayout";
+
 export default {
-    name: "Skills"
+    name: "Skills",
+    components: {
+        AppLayout
+    }
 }
 </script>
 
