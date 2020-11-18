82062428432cebf320608bdc595dd785883300eb
David <david@docampo.ch>
2020-11-18 23:17:57 +0100

    added vscode component and added c# to skills page

--- resources/js/Pages/Skills.vue
+++ resources/js/Pages/Skills.vue
@@ -23,7 +23,22 @@
             </div>
           </div>
 
-
+          <div class="w-full p-4">
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">Visual Studio Code</h3>
+                <vs-code-logo class="h-16"></vs-code-logo>
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 3 / 10</p>
+              </div>
+              <p class="mt-2">
+                For some appliances VSCode is more performant then PhpStorm e.g. Vue-CLI Apps. The indexing of PhpStorm
+                on my development Laptop is really slow, which is why i switch to VSCode if I have to do some intensive
+                Front-End tasks.
+              </p>
+            </div>
+          </div>
 
           <div class="w-full p-4">
             <h2 class="p-2 border-b text-2xl text-center border-gray-200">Programming Languages</h2>
@@ -44,6 +59,117 @@
             </div>
           </div>
 
+          <div class="w-full p-4">
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">C#</h3>
+                <c-sharp-logo class="w-24 h-24"></c-sharp-logo>
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 7 / 10</p>
+              </div>
+              <p class="mt-2">
+                I earned most of my experience of C# at my workplace. I wrote some smaller applications and add-ons
+                in C#. Everything I know about C# I learned myself from online resources.
+              </p>
+            </div>
+          </div>
+
+          <div class="w-full p-4">
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">Javascript</h3>
+                <c-sharp-logo class="w-24 h-24"></c-sharp-logo>
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 8 / 10</p>
+              </div>
+              <p class="mt-2">
+                Because I am mostly interested in
+              </p>
+            </div>
+          </div>
+
+          <div class="w-full p-4">
+            <h2 class="p-2 border-b text-2xl text-center border-gray-200">Frameworks</h2>
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">Laravel</h3>
+
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 7 / 10</p>
+              </div>
+              <p class="mt-2">
+
+              </p>
+            </div>
+          </div>
+
+          <div class="w-full p-4">
+            <h2 class="p-2 border-b text-2xl text-center border-gray-200">Databases</h2>
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">MySQL</h3>
+
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 6 / 10</p>
+              </div>
+              <p class="mt-2">
+
+              </p>
+            </div>
+          </div>
+
+          <div class="w-full p-4">
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">PostgreSQL</h3>
+
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 6 / 10</p>
+              </div>
+              <p class="mt-2">
+                Since most of my private projects are using Laravel, I am mostly developing with PHP. I am confident
+                that I am able to do realize most concepts with PHP. I will have to admit, that because I am doing a
+                lot with Laravel, my plain PHP skills have suffered a little, but I am confident i can pick it up
+                again quickly.
+              </p>
+            </div>
+          </div>
+
+          <div class="w-full p-4">
+            <h2 class="p-2 border-b text-2xl text-center border-gray-200">Other Technologies</h2>
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">Docker</h3>
+
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 6 / 10</p>
+              </div>
+              <p class="mt-2">
+
+              </p>
+            </div>
+          </div>
+
+          <div class="w-full p-4">
+            <div class="p-2">
+              <div class="flex justify-between mt-2">
+                <h3 class="text-center self-center font-semibold text-xl border-b border-gray-200">Git</h3>
+
+              </div>
+              <div class="my-auto font-semibold">
+                <p>Skill: 7 / 10</p>
+              </div>
+              <p class="mt-2">
+
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -54,13 +180,17 @@
 import AppLayout from "@/Layouts/AppLayout";
 import PhpStormLogo from '@/Shared/phpstorm-logo'
 import PhpLogo from '@/Shared/php-logo'
+import VsCodeLogo from '@/Shared/vs-code-logo'
+import CSharpLogo from '@/Shared/csharp-logo'
 
 export default {
     name: "Skills",
     components: {
         AppLayout,
         PhpStormLogo,
-        PhpLogo
+        PhpLogo,
+        VsCodeLogo,
+        CSharpLogo,
     }
 }
 </script>
---
+++ resources/js/Shared/vs-code-logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="/img/1920px-Visual_Studio_Code_1.35_icon.svg.png" alt="VSCode Logo">
+</template>
+
+<script>
+export default {
+    name: "VsCodeLogo"
+}
+</script>
+
+<style scoped>
+
+</style>
