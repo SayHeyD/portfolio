1a8fe0d93dbc48926f2f8ba274dc979f549d95b8
David <david@docampo.ch>
2020-11-19 18:09:35 +0100

    added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <link rel="icon" type="image/ico" href="/img/david-docampo-logo.ico">
+
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
