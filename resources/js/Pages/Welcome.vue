f9f1f9bc4e349626493b7c6e6c7e059a0485435f
David <david@docampo.ch>
2020-11-19 17:13:09 +0100

    added resume and hobbies to welcome page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -19,10 +19,20 @@
             </div>
             <div class="py-4">
               <p class="text-xl w-full text-center mb-2">What you can look at</p>
-              <div class="flex flex-col md:flex-row justify-around">
-                <inertia-link :href="route('skills')" class="bg-indigo-100 block p-4 shadow hover:underline hover:text-black hover:bg-indigo-300">
+              <div class="flex flex-col md:flex-row justify-center">
+                <inertia-link :href="route('skills')" class="bg-indigo-100 block p-4 mx-0 my-2 md:mx-2 md:my-0 shadow hover:underline hover:text-black hover:bg-indigo-300">
                   <svg focusable="false" data-prefix="fas" data-icon="list-ul" class="p-2 h-16 mx-auto text-gray-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M48 48a48 48 0 1 0 48 48 48 48 0 0 0-48-48zm0 160a48 48 0 1 0 48 48 48 48 0 0 0-48-48zm0 160a48 48 0 1 0 48 48 48 48 0 0 0-48-48zm448 16H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16zm0-320H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16V80a16 16 0 0 0-16-16zm0 160H176a16 16 0 0 0-16 16v32a16 16 0 0 0 16 16h320a16 16 0 0 0 16-16v-32a16 16 0 0 0-16-16z"></path></svg>
                   <p class="text-semibold w-full text-center text-black">My Skillset</p>
+                </inertia-link>
+
+                <inertia-link :href="route('hobbies')" class="bg-indigo-100 block p-4 mx-0 my-2 md:mx-2 md:my-0 shadow hover:underline hover:text-black hover:bg-indigo-300">
+                  <svg focusable="false" data-prefix="fas" data-icon="umbrella-beach" class="p-2 h-16 mx-auto text-gray-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M115.38 136.9l102.11 37.18c35.19-81.54 86.21-144.29 139-173.7-95.88-4.89-188.78 36.96-248.53 111.8-6.69 8.4-2.66 21.05 7.42 24.72zm132.25 48.16l238.48 86.83c35.76-121.38 18.7-231.66-42.63-253.98-7.4-2.7-15.13-4-23.09-4-58.02.01-128.27 69.17-172.76 171.15zM521.48 60.5c6.22 16.3 10.83 34.6 13.2 55.19 5.74 49.89-1.42 108.23-18.95 166.98l102.62 37.36c10.09 3.67 21.31-3.43 21.57-14.17 2.32-95.69-41.91-187.44-118.44-245.36zM560 447.98H321.06L386 269.5l-60.14-21.9-72.9 200.37H16c-8.84 0-16 7.16-16 16.01v32.01C0 504.83 7.16 512 16 512h544c8.84 0 16-7.17 16-16.01v-32.01c0-8.84-7.16-16-16-16z"></path></svg>
+                  <p class="text-semibold w-full text-center text-black">My Hobbies</p>
+                </inertia-link>
+
+                <inertia-link :href="route('resume')" v-if="$page.user !== null" class="bg-indigo-100 block p-4 mx-0 my-2 md:mx-2 md:my-0 shadow hover:underline hover:text-black hover:bg-indigo-300">
+                  <svg focusable="false" data-prefix="far" data-icon="file-alt" class="p-2 h-16 mx-auto text-gray-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill="currentColor" d="M288 248v28c0 6.6-5.4 12-12 12H108c-6.6 0-12-5.4-12-12v-28c0-6.6 5.4-12 12-12h168c6.6 0 12 5.4 12 12zm-12 72H108c-6.6 0-12 5.4-12 12v28c0 6.6 5.4 12 12 12h168c6.6 0 12-5.4 12-12v-28c0-6.6-5.4-12-12-12zm108-188.1V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V48C0 21.5 21.5 0 48 0h204.1C264.8 0 277 5.1 286 14.1L369.9 98c9 8.9 14.1 21.2 14.1 33.9zm-128-80V128h76.1L256 51.9zM336 464V176H232c-13.3 0-24-10.7-24-24V48H48v416h288z"></path></svg>
+                  <p class="text-semibold w-full text-center text-black">My Resume</p>
                 </inertia-link>
               </div>
               <p class="text-xl w-full text-center mt-12 mb-2">Socials</p>
