489b4082447b1530c90856e43420a008a7558762
David <david@docampo.ch>
2020-11-20 11:31:12 +0100

    added design to form

--- resources/js/Pages/Contact.vue
+++ resources/js/Pages/Contact.vue
@@ -9,7 +9,27 @@
     <div class="py-12">
       <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-          Contact
+          <p class="m-4 mb-0">You can contact me in english or in german. That way I will be able to reply most quickly.</p>
+          <form class="p-4" @submit.prevent="submit">
+            <div class="flex flex-col mb-4">
+              <label for="name">Name:</label>
+              <input id="name" name="name" class="mt-2 p-2 border-2 border-indigo-400 rounded focus:border-indigo-600 focus:outline-none" type="text" v-bind="form.name" required>
+            </div>
+
+            <div class="flex flex-col mb-4">
+              <label for="email">E-Mail:</label>
+              <input id="email" name="name" class="mt-2 p-2 border-2 border-indigo-400 rounded focus:border-indigo-600 focus:outline-none" type="email" v-bind="form.email" required>
+            </div>
+
+            <div class="flex flex-col mb-4">
+              <label for="message">Message:</label>
+              <textarea name="message" id="message" class="mt-2 p-2 border-2 border-indigo-400 rounded focus:border-indigo-600 focus:outline-none" cols="30" rows="10" v-bind="form.message" required></textarea>
+            </div>
+
+            <div class="w-full text-center">
+              <button type="submit" class="w-full md:w-1/3 p-4 text-xl text-white rounded bg-indigo-400 hover:underline hover:bg-indigo-600 transition-colors duration-300">Submit</button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
@@ -23,6 +43,24 @@ export default {
     name: "Contact",
     components: {
         AppLayout
+    },
+    data() {
+        return {
+            sending: false,
+            form: {
+                name: null,
+                email: null,
+                message: null,
+            }
+        }
+    },
+    methods: {
+        submit() {
+            this.$inertia.post(this.route('organizations.store'), this.form, {
+                onStart: () => this.sending = true,
+                onFinish: () => this.sending = false,
+            })
+        }
     }
 }
 </script>
