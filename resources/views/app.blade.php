8c0ca5bd9e4bcb8501354c51124c74e462d80d2e
David <david@docampo.ch>
2020-11-21 15:41:16 +0100

    grammar and adding og description

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <meta name="description" content="Welcome to my portfolio website. Feel free to explore, maybe you will even find some secrets"/>
+        <meta name="description" content="Welcome to my portfolio website. Feel free to explore, maybe you will even find some secrets."/>
 
         <meta property="og:title" content="Portfolio - David Docampo" />
+        <meta property="og:description" content="Welcome to my portfolio website. Feel free to explore, maybe you will even find some secrets.">
         <meta property="og:type" content="website" />
         <meta property="og:url" content="{{ env('APP_URL') }}" />
         <meta property="og:image" content="{{ asset('img/david-docampo-logo.png') }}" />
