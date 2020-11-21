dc56a9631c14a7f928ce180616a1c3de06f94e3f
David <david@docampo.ch>
2020-11-21 14:58:37 +0100

    added meta description tag

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,9 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="description" content="Welcome to my portfolio website. Feel free to explore, maybe you will even find some secrets"/>
+
         <link rel="icon" type="image/ico" href="/img/david-docampo-logo.ico">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>Portfolio - David Docampo</title>
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
