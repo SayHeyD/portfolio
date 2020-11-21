22d2f9221f6407fdb929ec5f09d5d9e845fe0af9
David <david@docampo.ch>
2020-11-21 14:21:43 +0100

    added error handling and changed applayout to be also used as error-page layout

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -40,9 +40,9 @@
                     <div v-if="$page.user !== null" class="hidden sm:flex sm:items-center sm:ml-6">
                         <div class="ml-3 relative">
                             <jet-dropdown align="right" width="48">
-                                <template #trigger>
+                                <template v-if="$page.user" #trigger>
                                     <button v-if="$page.jetstream.managesProfilePhotos" class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition duration-150 ease-in-out">
-                                        <img v-if="$page.user !== null" class="h-8 w-8 rounded-full object-cover" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
+                                        <img class="h-8 w-8 rounded-full object-cover" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
                                     </button>
 
                                     <button v-else class="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
@@ -56,7 +56,7 @@
                                     </button>
                                 </template>
 
-                                <template #content>
+                                <template v-if="$page.user" #content>
                                     <!-- Account Management -->
                                     <div class="block px-4 py-2 text-xs text-gray-400">
                                         Manage Account
@@ -73,7 +73,7 @@
                                     <div class="border-t border-gray-100"></div>
 
                                     <!-- Team Management -->
-                                    <template v-if="$page.jetstream.hasTeamFeatures">
+                                    <template v-if="$page.user && $page.jetstream.hasTeamFeatures">
                                         <div class="block px-4 py-2 text-xs text-gray-400">
                                             Manage Team
                                         </div>
@@ -99,7 +99,7 @@
                                                 <jet-dropdown-link as="button">
                                                     <div class="flex items-center">
                                                         <svg v-if="team.id == $page.user.current_team_id" class="mr-2 h-5 w-5 text-green-400" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-                                                        <div v-if="$page.user !== null">{{ team.name }}</div>
+                                                        <div v-if="$page.user">{{ team.name }}</div>
                                                         <div v-else>Guest</div>
                                                     </div>
                                                 </jet-dropdown-link>
@@ -162,10 +162,10 @@
                 </div>
 
                 <!-- Responsive Settings Options -->
-                <div v-if="$page.user !== null" class="pt-4 pb-1 border-t border-gray-200">
+                <div v-if="$page.user" class="pt-4 pb-1 border-t border-gray-200">
                     <div class="flex items-center px-4">
                         <div class="flex-shrink-0">
-                            <img v-if="$page.user !== null" class="h-10 w-10 rounded-full" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
+                            <img class="h-10 w-10 rounded-full" :src="$page.user.profile_photo_url" :alt="$page.user.name" />
                         </div>
 
                         <div class="ml-3">
---
+++ resources/js/Pages/Error.vue
@@ -0,0 +1,56 @@
+<template>
+  <app-layout>
+
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <div class="p-4">
+            <div class="py-2 border-b border-gray-200">
+              <h2 class="text-2xl font-semibold" v-text="'Error ' + title"></h2>
+            </div>
+
+            <div class="py-4">
+              <p v-text="description"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import AppLayout from "@/Layouts/AppLayout";
+
+export default {
+  name: "Error",
+    components: {
+      AppLayout
+    },
+    props: {
+        status: Number,
+    },
+    computed: {
+        title() {
+            return {
+                503: '503: Service Unavailable',
+                500: '500: Server Error',
+                404: '404: Page Not Found',
+                403: '403: Forbidden',
+            }[this.status]
+        },
+        description() {
+            return {
+                503: 'Sorry, we are doing some maintenance. Please check back soon.',
+                500: 'Whoops, something went wrong on our servers.',
+                404: 'Sorry, the page you are looking for could not be found.',
+                403: 'Sorry, you are forbidden from accessing this page.',
+            }[this.status]
+        },
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
